999f4ff40ae3265558ea8bb85d79d6be15d2ed1d
Nova <nova185@gmx.com>
2019-05-22 20:10:56 -0400

    Cooooode

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-pub mod ms;
-pub mod ss;
 pub mod boso;
 pub mod buso;
+pub mod ms;
+pub mod ss;
 extern crate rand;
 
 #[cfg(test)]
@@ -13,15 +13,16 @@ mod test_functions {
         //First we create a sorted vec.
         let mut sort_vec: Vec<usize> = (0..len).collect();
         let orig = sort_vec.clone();
-        
+
         sort_vec.shuffle(&mut thread_rng());
         let r = sort_vec.as_mut_slice();
 
         sort(r);
 
-        assert!(orig
-            .into_iter()
-            .zip(r.iter_mut())
-            .all(|(a, b)|a==*b), "{:?}", r)
+        assert!(
+            orig.into_iter().zip(r.iter_mut()).all(|(a, b)| a == *b),
+            "{:?}",
+            r
+        )
     }
 }
