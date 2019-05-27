b126e25cde3afdf12616caa14db2d92d3a966cb3
Nova <nova185@gmx.com>
2019-05-27 17:44:27 -0400

    Implemented insersion sort

---
+++ src/is.rs
@@ -0,0 +1,19 @@
+use std::cmp::{Ord, Ordering};
+
+#[bench]
+fn insertion_sort_properly_sorts(b: &mut test::Bencher) {
+    use crate::test_functions::sort_testing;
+    sort_testing(&insertion_sort, 10_000, b)
+}
+
+pub fn insertion_sort<T: Ord + Copy>(array: &mut [T]) {
+    for i in 1..array.len() {
+        for j in (1..=i).rev() {
+            if array[j].cmp(&array[j - 1]) == Ordering::Less {
+                array.swap(j, j - 1);
+            } else {
+                break;
+            }
+        }
+    }
+}
